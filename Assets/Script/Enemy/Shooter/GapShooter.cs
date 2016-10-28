41d3e33855a61cbfaf83a14a6f394ac30facb8fc
Nocha <Nocha@Nochaui-MacBook-Pro.local>
2016-10-29 08:10:42 +0900

    적기 추가

---
+++ Assets/Script/Enemy/Shooter/GapShooter.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GapShooter : MonoBehaviour {
+	public float angleRange;
+	public float speed;
+	public int count;
+	public int Interval;
+	private int Timer;
+	public GameObject bullet;
+	public bool canShoot = true;
+
+	void Update () {
+		if (Timer == 0 && canShoot)
+		{
+			EnemyLib.instance.ShootNWay(
+				transform.position, 
+				Random.Range(0.0f, 0.5f), 
+				angleRange, speed, count, 0, 0, bullet);
+		}
+		if (canShoot)
+			Timer = (Timer + 1) % Interval;
+		if (!canShoot)
+			Timer = 0;
+	}
+}
