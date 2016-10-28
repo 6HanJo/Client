715f0164bc4b954fe5fe93b4e596be5a29bfdbc5
YongJePark <haro7488@gmail.com>
2016-10-29 00:58:31 +0900

    ExamObjectPool

---
+++ Assets/Script/YongJe/ExamObjectPool/OPCubeCtrl.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using PathologicalGames;
+
+public class OPCubeCtrl : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnSpawned()
+    {
+        Debug.Log("Spawnd " + name);
+        StartCoroutine(DespawnedObject(5f));
+    }
+
+    IEnumerator DespawnedObject(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        PoolManager.Pools["Test"].Despawn(this.transform);
+    }
+
+    void OnDespawned()
+    {
+        Debug.Log("Despawned " + name);
+    }
+}
---
+++ Assets/Script/YongJe/ExamObjectPool/OPSpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+//오브젝트풀 사용을 위한 모듈 추가
+using PathologicalGames;
+
+public class OPSpawnManager : MonoBehaviour
+{
+    public Transform spawnPrefab;
+
+    //스폰딜레이 sec
+    public float delay = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(SpawnObject());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnObject()
+    {
+        yield return new WaitForSeconds(delay);
+        print(PoolManager.Pools["Test"].name);
+        Transform spawnInstance;
+        SpawnPool spawnPool = PoolManager.Pools["Test"];
+        spawnInstance = spawnPool.Spawn(spawnPrefab);
+        spawnInstance.position = new Vector3(10f, 0f, 0f);
+        StartCoroutine(SpawnObject());
+    }
+}
