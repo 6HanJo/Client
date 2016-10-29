7bd2c08f06f3223e4e891b054c3549f964d175dc
YongJePark <haro7488@gmail.com>
2016-10-29 16:34:03 +0900

    InGameManager EventSystem

--- Assets/Script/GameManager/InGameManager.cs
+++ Assets/Script/GameManager/InGameManager.cs
@@ -1,19 +1,33 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum PlayState
+{
+    None = -1,
+    Init,
+    Play,
+    BalanceAccounts,
+    Store
+}
+
 public class InGameManager : MonoBehaviour
 {
     public event CallbackGameBegin EventGameBegin;
     public event CallbackGameEnd EventGameEnd;
+    public event Callback EventPauseGame;
+    public event Callback EventResumeGame;
     public event Callback EventTimeLimitBegin;
     public event Callback EventTimeLimitEnd;
     public event Callback EventReBoot;
+    public event Callback EventBalanceAccounts;
+    public event Callback EventEnterStore;
+    public event Callback EventExitStore;
 
-    public static InGameManager instance;
-
-    public float maxTimeLimit;
-    float currentTimeLimit;
-    bool isTimeLimitUpdating;
+    static InGameManager instance;
+    public InGameManager Instance
+    {
+        get { return instance; }
+    }
 
     void Awake()
     {
@@ -23,21 +37,35 @@ public class InGameManager : MonoBehaviour
         }
     }
 
+    public float maxTimeLimit;
+    float leftTime;
+    bool isTimeLimitUpdating;
+
+    public PlayState playState;
+
+
     void Start()
     {
         //Event
         EventGameBegin += GameBegin;
         EventGameEnd += GameEnd;
+        EventPauseGame += PauseGame;
+        EventResumeGame += ResumeGame;
         EventTimeLimitBegin += TimeLimitBegin;
         EventTimeLimitEnd += TimeLimitEnd;
+        EventReBoot += ReBoot;
+        EventBalanceAccounts += BalanceAccounts;
+        EventEnterStore += EnterStore;
+        EventExitStore += ExitStore;
 
         Init();
     }
 
     void Init()
     {
-        currentTimeLimit = maxTimeLimit;
+        leftTime = maxTimeLimit;
         isTimeLimitUpdating = false;
+        playState = PlayState.Init;
     }
 
     void Update()
@@ -47,7 +75,10 @@ public class InGameManager : MonoBehaviour
 
     public void OnGameBegin()
     {
-        EventGameBegin();
+        if (EventGameBegin == null)
+            Debug.Log("EventGameBegin is Empty");
+        else
+            EventGameBegin();
     }
 
     void GameBegin()
@@ -62,7 +93,10 @@ public class InGameManager : MonoBehaviour
     
     public void OnGameEnd(bool isWin)
     {
-        EventGameEnd(isWin);
+        if (EventGameEnd == null)
+            Debug.Log("EventGameEnd is Empty");
+        else
+            EventGameEnd(isWin);
     }
 
     void GameEnd(bool isWin)
@@ -75,38 +109,131 @@ public class InGameManager : MonoBehaviour
         yield return null;
     }
 
+    public void OnPauseGame()
+    {
+        if (EventPauseGame == null)
+            Debug.Log("EventPauseGame is Empty");
+        else
+            EventPauseGame();
+    }
+
+    void PauseGame()
+    {
+        Debug.Log("PauseGame");
+    }
+
+    public void OnResumeGame()
+    {
+        if (EventResumeGame == null)
+            Debug.Log("EventResumeGame is Empty");
+        else
+            EventResumeGame();
+    }
+
+    void ResumeGame()
+    {
+        Debug.Log("ResumeGame");
+    }
+
     public void OnTimeLimitBegin()
     {
-        EventTimeLimitBegin();
+        if (EventTimeLimitBegin == null)
+            Debug.Log("EventTimeLimitBegin is Empty");
+        else
+        {
+            isTimeLimitUpdating = true;
+            EventTimeLimitBegin();
+        }
     }
 
     void TimeLimitBegin()
     {
         Debug.Log("TimeLimitBegin");
-        isTimeLimitUpdating = true;
         StartCoroutine(CoTimeLimitUpdate());
     }
 
     IEnumerator CoTimeLimitUpdate()
     {
         yield return null;
-        while(isTimeLimitUpdating)
+        while (isTimeLimitUpdating)
         {
-            currentTimeLimit -= Time.deltaTime;
             yield return null;
+            leftTime -= Time.deltaTime;
+            if (leftTime <= 0f)
+            {
+                OnTimeLimitEnd();
+            }
         }
     }
 
 
     public void OnTimeLimitEnd()
     {
-        EventTimeLimitEnd();
+        if(EventTimeLimitEnd == null)
+        {
+            Debug.Log("EventTimeLimitEnd is Empty");
+        }
+        else
+        {
+            isTimeLimitUpdating = false;
+            EventTimeLimitEnd();
+        }
     }
 
     void TimeLimitEnd()
     {
         Debug.Log("TimeLimitEnd");
-        isTimeLimitUpdating = false;
     }    
 
+    public void OnReBoot()
+    {
+        if (EventReBoot == null)
+            Debug.Log("EventReBoot is Empty");
+        else
+            EventReBoot();
+    }
+
+    void ReBoot()
+    {
+        Debug.Log("ReBoot");
+    }
+
+    public void OnBalanceAccounts()
+    {
+        if (EventBalanceAccounts == null)
+            Debug.Log("EventBalanceAccounts is Empty");
+        else
+            EventBalanceAccounts();
+    }
+
+    void BalanceAccounts()
+    {
+        Debug.Log("BalanceAccounts");
+    }
+
+    public void OnEnterStore()
+    {
+        if (EventEnterStore == null)
+            Debug.Log("EventEnterStore is Empty");
+        else
+            EventEnterStore();
+    }
+
+    void EnterStore()
+    {
+        Debug.Log("EnterStore");
+    }
+
+    public void OnExitStore()
+    {
+        if (EventExitStore == null)
+            Debug.Log("EventExitStore is Empty");
+        else
+            EventExitStore();
+    }
+
+    void ExitStore()
+    {
+        Debug.Log("ExitStore");
+    }
 }
