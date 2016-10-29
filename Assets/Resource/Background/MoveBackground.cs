998d3af538f14f74a57392653f791b506e8bcee7
Nocha <Nocha@Nochaui-MacBook-Pro.local>
2016-10-30 08:02:39 +0900

    움직이는 빽그라운드 ㅈㅔ작

---
+++ Assets/Resource/Background/MoveBackground.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveBackground : MonoBehaviour {
+
+	public float scrollSpeed = 0.01f;
+	public int backGroundType;
+
+	public Texture back1;
+	public Texture back2;
+	public Texture back3;
+
+	Material myMaterial;
+
+	void Start () {
+		myMaterial = GetComponent<Renderer> ().material;
+		if(backGroundType == 1)
+			myMaterial.mainTexture = back1;
+		else if(backGroundType == 2)
+			myMaterial.mainTexture = back2;
+		else if(backGroundType == 3)
+			myMaterial.mainTexture = back3;
+		myMaterial.mainTextureOffset = new Vector2 (0, 0);
+	}
+
+	void Update () { 
+		float newOffsetY = myMaterial.mainTextureOffset.y + scrollSpeed * Time.deltaTime;
+
+		myMaterial.mainTextureOffset = new Vector2 (0, newOffsetY);
+	}
+}
