4b57978897e94ed580af41578c2cfd020a00e97f
Nocha <Nocha@Nochaui-MacBook-Pro.local>
2016-10-29 09:42:06 +0900

    보스 패턴 추

--- Assets/Script/Enemy/Boss/Boss1.cs
+++ Assets/Script/Enemy/Boss/Boss1.cs
@@ -3,11 +3,39 @@ using System.Collections;
 
 public class Boss1 : MonoBehaviour {
 
-	void Start () {
-	
+	IntervalMultipleSpiralShooter interMul;
+	RandomSpreadingShooter randSpreading;
+	public Sprite Normal;
+	public Sprite Red;
+	SpriteRenderer sR;
+	bool isNormal;
+	void Awake()
+	{
+		sR = GetComponent<SpriteRenderer> ();
+		interMul = GetComponent<IntervalMultipleSpiralShooter> ();
+		randSpreading = GetComponent<RandomSpreadingShooter> ();
 	}
 
-	void Update () {
-	
+	void LevelUp()
+	{
+		randSpreading.ShotCount = 100;
+		interMul.shotCount = 100;
 	}
+
+	void Start () {
+		LevelUp ();
+	}
+
+	IEnumerator TrensformManage() {
+		while(true)
+		{
+			if (isNormal) {
+				sR.sprite = Red;
+			} else {
+				sR.sprite = Normal;
+			}
+			yield return new WaitForSeconds(30f);
+		}
+	}
+
 }
